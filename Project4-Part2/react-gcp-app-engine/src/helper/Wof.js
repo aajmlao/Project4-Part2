09c6d53267b4cc0ec091ef49d1106c246dfee8bc
Aaron Lao <91438480+aajmlao@users.noreply.github.com>
2023-12-13 09:50:14 -0800

    Update Wof.js

--- Project4-Part2/react-gcp-app-engine/src/helper/Wof.js
+++ Project4-Part2/react-gcp-app-engine/src/helper/Wof.js
@@ -1,4 +1,14 @@
 // guessing phrases. It was static array here. now change to the data from the cloud.
-export const targetPhrases =  localStorage.getItem("phrases").split(',')
+//export const targetPhrases =  localStorage.getItem("phrases").split(',')
 
-  
+export const targetPhrases =   [
+    "Spring",
+    "Tesla",
+    "USA",
+    "ChatBot",
+    "Man",
+    "Java",
+    "Basket",
+    "React",
+    "Phone",
+  ]
